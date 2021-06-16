d21beeaf2432488599b6a1a079dd6c5bade2e537
Dhaval1889 <dhaval1889@gmail.com>
2021-06-16 00:40:34 -0400

    POM file added
    POM file added

---
+++ src/main/java/Time_Tracker/App.java
@@ -0,0 +1,13 @@
+package Time_Tracker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
